0c47bcd42ec134f9fb98a7af3c2ac55a439cc597
MoJafa <jmohsa@gmail.com>
2023-03-11 21:10:04 +0200

    final keyword

---
+++ FinalKeyword/src/com/mojafa/Main.java
@@ -0,0 +1,10 @@
+package com.mojafa;
+
+public class Main {
+    public static final String NAME = "Mojafa";
+    public static void main(String[] args) {
+        //final var name = "Mojafa";
+        System.out.println(NAME);
+        System.out.println("Hello world!");
+    }
+}
