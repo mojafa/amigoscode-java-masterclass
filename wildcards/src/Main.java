876b304ffac8b7e4046b26e68a92386c094b547c
MoJafa <jmohsa@gmail.com>
2023-03-14 20:57:32 +0200

    unbounded and upper bounded wildcards

---
+++ wildcards/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Object> list1 = Arrays.asList("11",2,new Date());
+        List<Integer> list2 = Arrays.asList(1,2,3,4,5);
+        List<String> list3 = Arrays.asList("a","b","c","d","e");
+        print(list1);
+        print(list2);
+        print(list3);
+        //upper bounded wildcard
+        // here we can work with number Integer
+        print2(list2);
+    }
+
+    //unbounded wildcard ?
+    static void print (List<?> list){
+        //list.forEach(System.out::println);
+        list.forEach(e -> {
+                    System.out.println(e.getClass().getName());
+                    System.out.println(e);
+
+                });
+    } 
+
+    //upper bounded wildcard
+    static void print2 (List<? extends Number> list){
+        list.forEach(System.out::println);
+    }
+}
